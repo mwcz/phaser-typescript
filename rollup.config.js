447a3d95a11f01a86d97b1b7c7098c0934f24a49
mwcz <mwc@clayto.com>
2016-10-03 10:17:15 -0400

    update build/watch scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,33 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
+import path from 'path';
+import rollupNodeResolve from 'rollup-plugin-node-resolve';
+import rollupCommonJS from 'rollup-plugin-commonjs';
+import rollupInject from 'rollup-plugin-inject';
 
 export default {
-    entry: 'app.js',
-    dest: 'app.min.js',
+    entry: 'build/app.js',
+    dest: 'build/app.min.js',
     sourceMap: true,
-    sourceMapFile: 'app.js.map',
+    sourceMapFile: 'build/app.js.map',
     format: 'iife',
     context: 'window',
     exports: 'none',
-    external: [
-        'phaser',
+    treeshake: false,
+    plugins: [
+        rollupNodeResolve({
+            jsnext: true,
+            browser: true,
+        }),
+        // rollupInject({
+        //     // include: '**/*.js',
+        //     modules: {
+        //         Phaser: path.resolve('node_modules/phaser/build/phaser.js'),
+        //     },
+        // }),
     ],
-    globals: {
-        phaser: 'Phaser',
-    },
+    // external: [
+    //     'phaser',
+    // ],
+    // globals: {
+    //     phaser: 'Phaser',
+    // },
 };
