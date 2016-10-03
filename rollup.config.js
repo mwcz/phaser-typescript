108e00a2af2c568829333ac7e4b9e26fd660edcb
mwcz <mwc@clayto.com>
2016-10-03 15:25:50 -0400

    add lodash to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -14,23 +14,25 @@ export default {
     treeshake: true,
     external: [
         'phaser',
+        'lodash',
     ],
     globals: {
         phaser: 'Phaser',
+        lodash: '_',
     },
     // These plugins were used to inject Phaser into the build, but this slowed
     // the build down a lot so I switched to loading it from a CDN.  Might be
     // useful for something in the future though.
     // plugins: [
-    //     // rollupNodeResolve({
-    //     //     jsnext: true,
-    //     //     browser: true,
-    //     // }),
-    //     // rollupInject({
-    //     //     // include: '**/*.js',
-    //     //     modules: {
-    //     //         Phaser: path.resolve('node_modules/phaser/build/phaser.js'),
-    //     //     },
-    //     // }),
+    //     rollupNodeResolve({
+    //         jsnext: true,
+    //         browser: true,
+    //     }),
+    //     rollupInject({
+    //         include: '**/*.js',
+    //         // modules: {
+    //         //     Phaser: path.resolve('node_modules/phaser/build/phaser.js'),
+    //         // },
+    //     }),
     // ],
 };
