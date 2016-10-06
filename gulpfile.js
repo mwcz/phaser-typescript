99d1a870cc2dfaa3c15b44038eb9a80404aabdba
mwcz <mwc@clayto.com>
2016-10-06 11:59:52 -0400

    remove old gulp-watch argument

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,8 @@ const OUT_DIR = 'build';
 const TS_FILES = [`${SRC_DIR}/**/*.ts`];
 const OTHER_FILES = [`${SRC_DIR}/**/*`, `!${SRC_DIR}/**/*.ts`];
 
-function copyTask(event) {
-    const path = event.path || OTHER_FILES;
-    return gulp.src(path, { base: SRC_DIR }).pipe(gulp.dest(OUT_DIR));
+function copyTask() {
+    return gulp.src(OTHER_FILES, { base: SRC_DIR }).pipe(gulp.dest(OUT_DIR));
 }
 
 function watchTask() {
